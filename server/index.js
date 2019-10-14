9a2c2bf71350d87bcea5c059647d066ed8276ecb
Ivo Breukers <ivo@breukers.me>
2019-10-14 15:08:00 +0200

    add server packages and setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const PORT = 4000;
+const DATABASE_NAME = 'quizzer';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(`mongodb://127.0.0.1:27017/${DATABASE_NAME}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+  console.log('MongoDB database connection established successfully');
+});
+
+app.listen(PORT, function() {
+  console.log('Server is running on Port: ' + PORT);
+});
