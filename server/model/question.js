5750dc98bfb811743197636675c87a68595f77f2
Oktay Dinler <24858380+aoktayd@users.noreply.github.com>
2019-10-14 16:25:18 +0200

    add seeding for mongodb

---
+++ server/model/question.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Question = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  language: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Question', Question);
