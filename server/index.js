48167322f0b7521ee4ac6f22d667b052e3ddf3d5
Oktay Dinler <24858380+aoktayd@users.noreply.github.com>
2019-10-16 14:19:24 +0200

    add express-session with mongodb store

--- server/index.js
+++ server/index.js
@@ -3,14 +3,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const http = require('http');
+
 const simpleErrorHandler = require('./middleware/error-handler');
 const useAcceptLanguage = require('./middleware/accept-language');
 
-const { EXPRESS_PORT, MONGODB_HOST, MONGODB_PORT, DB_NAME } = process.env;
+const { EXPRESS_PORT, EXPRESS_SECRET, MONGODB_HOST, MONGODB_PORT, DB_NAME } = process.env;
+
 const app = express();
+const httpServer = http.createServer(app);
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(
+  session({
+    saveUninitialized: false, // Don't create session until something stored
+    resave: false, // Don't save session if unmodified
+    secret: EXPRESS_SECRET,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+    }),
+  })
+);
 app.use(useAcceptLanguage());
 
 require('./models');
@@ -32,6 +48,6 @@ mongoose.connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
 
-app.listen(EXPRESS_PORT, () => {
+httpServer.listen(EXPRESS_PORT, () => {
   console.log(`Server is running on Port: ${EXPRESS_PORT}`);
 });
