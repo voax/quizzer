2450cfefc6e03040b404e081b6ac81d3010632fc
Oktay Dinler <dinler@pm.me>
2019-10-23 14:16:01 +0200

    update server index.js bootstrapping

--- server/src/index.js
+++ server/src/index.js
@@ -6,30 +6,35 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const http = require('http');
+const ws = require('ws');
+
+require('./models');
 
 const simpleErrorHandler = require('./middleware/error-handler');
 const useAcceptLanguage = require('./middleware/accept-language');
+const httpWsUpgrade = require('./middleware/http-ws-upgrade');
+const { initRoomCodes } = require('./rooms/code');
 
 const { EXPRESS_PORT, EXPRESS_SECRET, MONGODB_HOST, MONGODB_PORT, DB_NAME } = process.env;
 
 const app = express();
 const httpServer = http.createServer(app);
+const wss = new ws.Server({ noServer: true });
+const store = new MongoStore({
+  mongooseConnection: mongoose.connection,
+});
+const sessionParser = session({
+  saveUninitialized: false, // Don't create session until something stored
+  resave: false, // Don't save session if unmodified
+  secret: EXPRESS_SECRET,
+  store,
+});
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(
-  session({
-    saveUninitialized: false, // Don't create session until something stored
-    resave: false, // Don't save session if unmodified
-    secret: EXPRESS_SECRET,
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-    }),
-  })
-);
+app.use(sessionParser);
 app.use(useAcceptLanguage());
 
-require('./models');
 require('./routes')(app);
 
 app.use(
@@ -39,15 +44,24 @@ app.use(
   })
 );
 
-mongoose.connect(`mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${DB_NAME}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+httpServer.on('upgrade', httpWsUpgrade(sessionParser)(wss));
+require('./wss.js')(wss);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
+const main = async () => {
+  await mongoose.connect(`mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await initRoomCodes();
+  await new Promise(resolve => {
+    httpServer.listen(EXPRESS_PORT, resolve);
+  });
+};
 
-httpServer.listen(EXPRESS_PORT, () => {
-  console.log(`Server is running on Port: ${EXPRESS_PORT}`);
-});
+main()
+  .then(() => {
+    console.log(`Server is running on Port: ${EXPRESS_PORT}`);
+  })
+  .catch(reason => {
+    console.error('ERROR:', reason);
+  });
