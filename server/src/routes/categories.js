3a247440cdd250325fbb1225f824d32bee598d2c
Oktay Dinler <dinler@pm.me>
2019-10-24 09:08:26 +0200

    refactor categories router, cleanup/simplify

--- server/src/routes/categories.js
+++ server/src/routes/categories.js
@@ -7,25 +7,13 @@ const Question = mongoose.model('Question');
 const catchErrors = require('../middleware/catch-errors');
 const { QM } = require('./roles');
 
-router.use('/', (req, res, next) => {
-  req.mongoQueryAggregate = { $match: {} };
-  req.mongoQueryProjection = { _id: 0, __v: 0 };
-
-  if (req.language.length > 0) {
-    req.mongoQueryAggregate.$match = { language: req.language[0].code };
-  }
-
-  next();
-});
-
 router.get(
   '/',
   catchErrors(async (req, res) => {
-    const categories = await Question.aggregate([
-      { ...req.mongoQueryAggregate },
-      { $group: { _id: '$category' } },
-    ]);
-    res.json(categories.map(({ _id }) => _id));
+    const categories = await Question.find({ ...req.firstLanguage() })
+      .distinct('category')
+      .exec();
+    res.json(categories);
   })
 );
 
@@ -35,9 +23,7 @@ router.get(
     if (req.session.role !== QM) {
       return res.status(401).json({ message: "You're not welcome here..." });
     }
-    res.json(
-      await Question.find({ category: req.params.category }, { ...req.mongoQueryProjection })
-    );
+    res.json(await Question.find({ category: req.params.category }, { __v: 0 }));
   })
 );
 
