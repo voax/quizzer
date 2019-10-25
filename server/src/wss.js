d5c3ff18494217fee4f4dcf9c780ea819c9682c5
Oktay Dinler <dinler@pm.me>
2019-10-25 10:28:35 +0200

    server: keep track of sockets and add ping pong for TEAM_APPLIED

--- server/src/wss.js
+++ server/src/wss.js
@@ -1,9 +1,27 @@
 module.exports = wss => {
+  const sockets = new Map();
+
   wss.on('connection', (socket, request) => {
-    console.log('New connection!', request.session.id);
+    const { id, room } = request.session;
+    console.log('New connection!', id);
+    sockets.set(id, socket);
 
     socket.on('message', data => {
       console.log('Data received:', data);
+
+      const { command } = JSON.parse(data);
+      switch (command) {
+        case 'TEAM_APPLIED':
+          sockets[room.host].send(command);
+          return;
+        default:
+          return;
+      }
+    });
+
+    socket.on('close', (code, reason) => {
+      console.log('Connection closed!', code, reason);
+      sockets.delete(id);
     });
   });
 };
