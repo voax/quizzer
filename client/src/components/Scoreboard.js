5ef0ff023a04c93d0118d99c73ba46bf5d5245a8
Oktay Dinler <dinler@pm.me>
2019-10-30 10:23:25 +0100

    clean scoreboard codebase, start from scratch

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ScoreboardHome from './ScoreboardHome';
-import ScoreboardLogin from './ScoreboardLogin';
 
 const Scoreboard = () => {
   return (
@@ -10,9 +9,6 @@ const Scoreboard = () => {
         path="/scoreboard/:roomCode"
         render={routeProps => <ScoreboardHome {...routeProps} />}
       />
-      <Route path="/scoreboard">
-        <ScoreboardLogin />
-      </Route>
     </Switch>
   );
 };
--- client/src/components/ScoreboardHome.js
+++ client/src/components/ScoreboardHome.js
@@ -1,24 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-// import { fetchQuestions } from '../../reducers/qustions';
-import ScoreboardItem from './ScoreboardItem';
 
-const ScoreboardHome = ({ match }) => {
-  const scoreboard = useSelector(state => state.scoreboard);
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   dispatch(fetchQuestions());
-  // }, []);
-
-  console.log(match);
-  return (
-    <>
-      {scoreboard.map(({ team, score }) => (
-        <ScoreboardItem key={team} team={team} score={score} />
-      ))}
-    </>
-  );
+const ScoreboardHome = () => {
+  return <h1>Here come's the Quizzer Scoreboard</h1>;
 };
 
 export default ScoreboardHome;
