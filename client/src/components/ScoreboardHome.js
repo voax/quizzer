fff69fa77c83a3e836e565b16ef0ff59fa8735e7
Oktay Dinler <dinler@pm.me>
2019-10-30 11:16:25 +0100

    scoreboard can now connect to a room

--- client/src/components/ScoreboardHome.js
+++ client/src/components/ScoreboardHome.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container, Row, Col } from 'react-grid-system';
+import { Redirect } from 'react-router-dom';
 
-const ScoreboardHome = () => {
-  return <h1>Here come's the Quizzer Scoreboard</h1>;
+import { loginAsScoreboardViewer } from '../reducers/scoreboard';
+import Loader from './Loader';
+
+const CenterLoader = () => {
+  return (
+    <Container fluid className="full-screen center">
+      <Row className="focus-center">
+        <Col>
+          <Loader />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+const ScoreboardHome = ({
+  match: {
+    params: { roomCode },
+  },
+}) => {
+  const dispatch = useDispatch();
+  const sbRoomCode = useSelector(state => state.scoreboard.roomCode);
+  const triedConnectingToRoom = useSelector(state => state.scoreboard.triedConnectingToRoom);
+  const connectedToRoom = useSelector(state => state.scoreboard.connectedToRoom);
+  const connectingToRoom = useSelector(state => state.scoreboard.connectingToRoom);
+
+  useEffect(() => {
+    if (!connectingToRoom && !triedConnectingToRoom) {
+      dispatch(loginAsScoreboardViewer(roomCode));
+    }
+  }, [dispatch, roomCode, connectingToRoom, triedConnectingToRoom]);
+
+  if (!triedConnectingToRoom) {
+    return <CenterLoader />;
+  }
+  if (!connectedToRoom) {
+    return <Redirect to="/" />;
+  }
+
+  return <h1>{sbRoomCode} Here come's the Quizzer Scoreboard</h1>;
 };
 
 export default ScoreboardHome;
--- client/src/reducers/scoreboard.js
+++ client/src/reducers/scoreboard.js
@@ -1,8 +1,52 @@
 import produce from 'immer';
 
-export default produce((draft, action) => {
-  switch (action.type) {
-    default:
-      return;
+import { fetchApi, checkFetchError } from '../utils';
+import { showPopUpAction } from './pop-up';
+import { setLoaderAction, stopLoaderAction } from './loader';
+
+export const loginAsScoreboardViewer = roomCode => async dispatch => {
+  try {
+    dispatch({ type: 'STARTED_CONNECTING_TO_ROOM' });
+    dispatch(setLoaderAction('Connecting to room...'));
+
+    const response = await fetchApi(`rooms/${roomCode}/scoreboards`, 'POST');
+    const { message } = await checkFetchError(response);
+    dispatch(setLoaderAction(message));
+
+    dispatch({ type: 'ACCESS_CONFIRMED', roomCode });
+  } catch (error) {
+    dispatch(showPopUpAction('ERROR', error.message));
+  } finally {
+    dispatch({ type: 'TRIED_CONNECTING_TO_ROOM' });
+    dispatch({ type: 'STOPPED_CONNECTING_TO_ROOM' });
+    dispatch(stopLoaderAction());
   }
-}, {});
+};
+
+export default produce(
+  (draft, action) => {
+    switch (action.type) {
+      case 'ACCESS_CONFIRMED':
+        draft.roomCode = action.roomCode;
+        draft.connectedToRoom = true;
+        return;
+      case 'TRIED_CONNECTING_TO_ROOM':
+        draft.triedConnectingToRoom = true;
+        return;
+      case 'STARTED_CONNECTING_TO_ROOM':
+        draft.connectingToRoom = true;
+        return;
+      case 'STOPPED_CONNECTING_TO_ROOM':
+        draft.connectingToRoom = false;
+        return;
+      default:
+        return;
+    }
+  },
+  {
+    roomCode: null,
+    triedConnectingToRoom: false,
+    connectingToRoom: false,
+    connectedToRoom: false,
+  }
+);
